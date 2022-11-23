68e5b93a38c955c84a738e0a9a7965ae3be52ea1
FelipeQG <fquiceno1@misena.edu.co>
2022-11-22 20:05:31 -0500

    Modales de Todo y sistema examanes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const openModal = document.querySelector('.todo-open');
+//Abrir modal de todo list
+const openModalTodoList = document.querySelector('.todo-open');
 const modalTodo = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal-close')
 
-openModal.addEventListener('click', (e) =>{
+openModalTodoList.addEventListener('click', (e) =>{
     e.preventDefault();
     modalTodo.classList.add('modal-show');
     
@@ -11,4 +12,20 @@ openModal.addEventListener('click', (e) =>{
 closeModal.addEventListener('click',(e)=>{
     e.preventDefault();
     modalTodo.classList.remove('modal-show');
+});
+
+//Abrir modal de Sistema exámenes
+const openModalExams = document.querySelector('.exams-open');
+const modalExam = document.querySelector('.modal-exam');
+const closeModalExam = document.querySelector('.modal-close-exam');
+
+
+openModalExams.addEventListener('click', (e) =>{
+    e.preventDefault();
+    modalExam.classList.add('modal-exam-show');
+});
+
+closeModalExam.addEventListener('click',(e)=>{
+    e.preventDefault();
+    modalExam.classList.remove('modal-exam-show');
 });
